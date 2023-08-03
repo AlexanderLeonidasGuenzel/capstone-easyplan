0fc7aa1155f3b0f8a92376cc8be1bd38cb26af35
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-08-03 16:11:07 +0200

    add form for new plan and send it to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,22 @@
 import './App.css'
+import {ChangeEvent, useState} from "react";
+import axios from "axios";
 
 function App() {
+    const[planInput, setPlanInput] = useState("");
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        setPlanInput(event.target.value);
+    }
+    function handleSubmit(event: ChangeEvent<HTMLFormElement>)  {
+        event.preventDefault();
+        axios.post('/api/plan', {
+            name: planInput
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+        setPlanInput("");
+    }
 
   return (
     <>
@@ -10,8 +26,17 @@ function App() {
         There are no existing plans!
       </p>
         <button>new plan</button>
+        <form onSubmit={handleSubmit}>
+            <label>name </label>
+            <input type="text" placeholder={"week-1"} value={planInput} onChange={handleChange}/>
+            <button>add</button>
+        </form>
     </>
   )
 }
+
+
+
+
 
 export default App
