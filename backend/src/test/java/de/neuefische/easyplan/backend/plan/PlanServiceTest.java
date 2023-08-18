a6c52d11c01ad09a1dadf4a549d60f473dbc2130
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-08-18 02:19:19 +0200

    service unit test

--- backend/src/test/java/de/neuefische/easyplan/backend/plan/PlanServiceTest.java
+++ backend/src/test/java/de/neuefische/easyplan/backend/plan/PlanServiceTest.java
@@ -1,13 +1,14 @@
 package de.neuefische.easyplan.backend.plan;
 
 import de.neuefische.easyplan.backend.utils.IDGenerator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class PlanServiceTest {
@@ -32,7 +33,7 @@ class PlanServiceTest {
         when(planRepository.save(expected)).thenReturn(expected);
         Plan actual = planService.addPlan(planData);
         //THEN
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
         verify(planRepository).save(expected);
     }
 
@@ -44,7 +45,7 @@ class PlanServiceTest {
         //WHEN
         when(planRepository.findAll()).thenReturn(List.of(plan1, plan2));
         //THEN
-        assertEquals(List.of(plan1, plan2), planService.getAllPlans());
+        Assertions.assertEquals(List.of(plan1, plan2), planService.getAllPlans());
         verify(planRepository).findAll();
     }
 
@@ -55,12 +56,12 @@ class PlanServiceTest {
         //WHEN
         when(planRepository.findById("1")).thenReturn(Optional.of(plan1));
         //THEN
-        assertEquals(plan1, planService.getPlanById("1"));
+        Assertions.assertEquals(plan1, planService.getPlanById("1"));
         verify(planRepository).findById("1");
     }
 
     @Test
-    void getPlandByIdShouldThrowExceptionIfPlanDoesNotExist() {
+    void getPlanByIdShouldThrowExceptionIfPlanDoesNotExist() {
         //GIVEN
         //WHEN
         when(planRepository.findById("1")).thenReturn(Optional.empty());
@@ -86,8 +87,44 @@ class PlanServiceTest {
         planService.editPlan(id, planData);
 
         // Assert
-        assertEquals(planData.getName(), existingPlan.getName());
+        Assertions.assertEquals(planData.getName(), existingPlan.getName());
         verify(planRepository).save(existingPlan);
     }
 
+    @Test
+    void deletePlanShouldDeletePlanWithGivenId() {
+        //GIVEN
+        Plan plan1 = new Plan("1", "planName1");
+        //WHEN
+        when(planRepository.findById("1")).thenReturn(Optional.of(plan1));
+        //THEN
+        planService.deletePlan("1");
+        verify(planRepository).deleteById("1");
+    }
+
+    @Test
+    void givenPlanNotEqualsExpectedPlanAfterDelete() {
+
+        //GIVEN
+        Plan plan1 = new Plan("1", "planName1");
+        Plan plan2 = new Plan("2", "planName2");
+        Plan plan3 = new Plan("3", "planName3");
+        List<Plan> givenPlan = List.of(plan1, plan2, plan3);
+        List<Plan> expectedListAfterDelete = List.of(plan2, plan3);
+
+        //WHEN
+        when(planRepository.findAll()).thenReturn(expectedListAfterDelete);
+
+        //THEN
+        planService.deletePlan("1");
+
+        List<Plan> actualListAfterDelete = planService.getAllPlans();
+
+        Assertions.assertNotEquals(givenPlan, actualListAfterDelete);
+        Assertions.assertEquals(expectedListAfterDelete, actualListAfterDelete);
+
+        verify(planRepository).findAll();
+        verify(planRepository).deleteById("1");
+    }
 }
+
