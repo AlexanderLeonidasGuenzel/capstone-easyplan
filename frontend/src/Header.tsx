e475fe4d3122ff3ff91265f2a8630e02b5fe72de
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-08-18 21:25:41 +0200

    add header component

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+export default function Header() {
+
+  return (
+    <>
+        <div className="header">
+        <h1>Plan your day</h1>
+        </div >
+    </>
+  )
+}
+
