712bead13800a4538481cb66e55c573e6f707448
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-08-20 01:44:58 +0200

    footer and header text design, font

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -5,7 +5,11 @@ export default function Header() {
   return (
     <>
         <div className="header">
-        <h1>EasyPlan</h1><h3>Smart scheduling</h3>
+            <div className="header-text">
+                <h1>EasyPlan</h1>
+                <h3>Smart scheduling</h3>
+            </div>
+
         </div >
     </>
   )
