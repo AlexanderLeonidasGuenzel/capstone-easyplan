7bb4e2b76de098770cb4a699d29ccecbaf3f4986
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-08-20 00:17:40 +0200

    footer and header text design

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -2,10 +2,18 @@ import './Footer.css'
 
 export default function Footer() {
 
+    const myName = "Alexander Leonidas Guenzel"
+    const company = "Neue Fische GmbH"
+    const year = new Date().getFullYear();
+    const course = "Java Fullstack Bootcamp";
+    const line = " | ";
+
   return (
     <>
         <div className="footer">
-        <p>Author: Alexander Leonidas Guenzel</p>
+            <p><span className="orange">©{year}</span> {myName.toUpperCase()}
+                <span className="orange">{line}{company.toUpperCase()}{line}</span>
+                {course.toUpperCase()}</p>
         </div >
     </>
   )
--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
         <div className="header">
-        <h1>EasyPlan</h1>
+        <h1>EasyPlan</h1><h3>Smart scheduling</h3>
         </div >
     </>
   )
